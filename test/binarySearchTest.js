76befdd995981a863ea1b3d4bbf0421d88101f34
j2FunOnly <j2funonly@ya.ru>
2019-06-07 10:57:17 +0300

    Fix typo

--- test/binarySearchTest.js
+++ test/binarySearchTest.js
@@ -3,14 +3,14 @@ const assert = require('assert');
 const binarySearch = require('../lib/binarySearch.js');
 
 describe('binarySearch', () => {
-  it ('return index of found element', () => {
+  it ('return index of the item found', () => {
     const target = 4;
     const arr = [1, 2, 4, 5, 8];
 
     assert.equal(binarySearch(target, arr), 2);
   });
 
-  it ('return "-1" if element not found', () => {
+  it ('return "-1" if item not found', () => {
     const target = 3;
     const arr = [1, 2, 4, 5, 8];
 
